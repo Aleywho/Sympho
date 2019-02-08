8df3cc0862a82f074d1ed6aeaf56f803dc39e7dd
Aleywho <alexcurot@hotmail.com>
2019-02-08 11:20:30 +0100

    Nouvelle entity

---
+++ src/Entity/Visiteurs.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\VisiteursRepository")
+ */
+class Visiteurs
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Username;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $Birthdate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Pays;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->Username;
+    }
+
+    public function setUsername(string $Username): self
+    {
+        $this->Username = $Username;
+
+        return $this;
+    }
+
+    public function getBirthdate(): ?\DateTimeInterface
+    {
+        return $this->Birthdate;
+    }
+
+    public function setBirthdate(\DateTimeInterface $Birthdate): self
+    {
+        $this->Birthdate = $Birthdate;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->Pays;
+    }
+
+    public function setPays(string $Pays): self
+    {
+        $this->Pays = $Pays;
+
+        return $this;
+    }
+}
