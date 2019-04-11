870305d8f7c9cfba691357b0cd9b639e9a22352d
Aleywho <alexcurot@hotmail.com>
2019-04-11 13:54:12 +0200

    Tarif

--- src/Service/TarifGenerator.php
+++ src/Service/TarifGenerator.php
@@ -4,13 +4,19 @@ namespace App\Service;
 
 class TarifGenerator
 {
-    public function getTarif($command)
+    public function getTarif($command, $datedenaissance)
     {
         dump($command);
-        //créer la variables date du jour, $now = new \DateTime()
+
+        $now = new \DateTime();
+        $age1 =$datedenaissance->diff($now);
+        $age = $age1;
+
+
 
         foreach ($command->getBillets() as $billet)
         {
+        $billet->getDatedenaissance;
 
             //Récupère la date de naissance du billet $billet->getDatedenaissance
             //On compare now, avec date de naissance et obtient de le nombre d'année de diffèrence, if else
