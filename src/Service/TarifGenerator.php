4b96cef3c92b44d7cc0c11013cf6ecc1cb8105a8
Aleywho <alexcurot@hotmail.com>
2019-04-16 01:00:03 +0200

    tarif

--- src/Service/TarifGenerator.php
+++ src/Service/TarifGenerator.php
@@ -17,13 +17,26 @@ class TarifGenerator
        $datedenaissance = $billet->getDatedenaissance() -> diff($now);
        $difference = $datedenaissance->format('\'%y\'');
 dump($difference);
-//if $difference
+if ($difference < 4) {
+            $billet = 0;
+        } elseif ($difference < 13) {
+        $billet = 8;
+        } elseif ($reduction == true) {
+        $billet = 10;
+        } elseif ($difference > 12 && $difference < 60) {
+        $billet = 16;
+        } else {
+            $billet = 12;
+        }
+        return ($billet);
+    }
 
-            //On compare now, avec date de naissance et obtient de le nombre d'année de diffèrence, if else
+
+    //On compare now, avec date de naissance et obtient de le nombre d'année de diffèrence, if else
             //Une fois qu'on a trouvé le tarif avec if else, on appelle la méthode  $billet -> setPrix($rezul)
         }
            // $rez = 4;
 
         //Fixer le total, grace à la méthode setTotal()$command->setTotal();
-    }
+
 }
